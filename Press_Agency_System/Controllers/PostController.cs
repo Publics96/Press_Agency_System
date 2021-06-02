a0fd3203c00958845f9f0fedf7033c7f4fcc1a93
Mohamed11Ahmed <mohamed.1911.ahmed@gmail.com>
2021-06-02 13:42:11 +0200

    adding post details

--- Press_Agency_System/Controllers/PostController.cs
+++ Press_Agency_System/Controllers/PostController.cs
@@ -29,7 +29,7 @@ namespace Press_Agency_System.Controllers
         public ActionResult AllPosts()
         {
             db.Configuration.ProxyCreationEnabled = false;
-            var posts = db.Posts.Include(x=> x.User);
+            var posts = db.Posts.Include(x => x.User);
             return Json(new { data = posts }, JsonRequestBehavior.AllowGet);
 
         }
@@ -55,12 +55,38 @@ namespace Press_Agency_System.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Post post = db.Posts.Find(id);
+            Post post = db.Posts.Include(e => e.User).SingleOrDefault(x => x.Id == id);
+
             if (post == null)
             {
                 return HttpNotFound();
             }
-            return View(post);
+            var userId = User.Identity.GetUserId();
+            List<InteractedPosts> allposts = db.IneractedPosts.Where(e => e.PostId == id).ToList();
+            var obj = allposts.Find(e => e.UserId == userId) as InteractedPosts;
+            byte likedbyuser;
+            if (obj == null)
+                likedbyuser = 0;
+            else
+                likedbyuser = obj.Like;
+
+
+            var objsaved = db.SavedPosts.FirstOrDefault(e => e.PostId == id && e.UserId == userId) as SavedPosts;
+            var savedbyuser = false;
+            if (objsaved != null)
+            {
+                savedbyuser = true;
+            }
+
+            PostDetailsViewModel pdvm = new PostDetailsViewModel()
+            {
+                allInteractions = allposts,
+                Post = post,
+                likedbyuser = likedbyuser,
+                saveedbyuser = savedbyuser
+            };
+
+            return View(pdvm);
         }
 
         // GET: /Post/Create
@@ -71,7 +97,7 @@ namespace Press_Agency_System.Controllers
         }
 
         // POST: /Post/Create
-     
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Post post, HttpPostedFileBase UploadedImage)
@@ -122,7 +148,7 @@ namespace Press_Agency_System.Controllers
         }
 
         // POST: /Post/Edit/5
- 
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(Post post, HttpPostedFileBase UploadedImage)
@@ -171,7 +197,35 @@ namespace Press_Agency_System.Controllers
 
         }
 
- 
+        public ActionResult ViewPost(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            var react = new InteractedPosts();
+            react.PostId = post.Id;
+            react.UserId = User.Identity.GetUserId();
+            react.Like = 0;
+            var interactedpost = db.IneractedPosts.FirstOrDefault(x => x.UserId == react.UserId && x.PostId == react.PostId) as InteractedPosts;
+            if (interactedpost != null)
+            {
+                return Content("0");
+            }
+            else
+            {
+                db.IneractedPosts.Add(react);
+                db.SaveChanges();
+                return Content("1");
+            }
+
+        }
+
 
         public ActionResult Like(int? id)
         {
@@ -188,11 +242,120 @@ namespace Press_Agency_System.Controllers
             react.PostId = post.Id;
             react.UserId = User.Identity.GetUserId();
             react.Like = 1;
-            db.IneractedPosts.Add(react);
-            db.SaveChanges();
+            var interactedpost = db.IneractedPosts.FirstOrDefault(x => x.UserId == react.UserId && x.PostId == react.PostId);
+            if (interactedpost != null)
+            {
+                if (interactedpost.Like == 2)
+                {
+                    interactedpost.Like = 1;
+                    db.Entry(interactedpost).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Content("1");
+                }
+                else if (interactedpost.Like == 0)
+                {
+                    interactedpost.Like = 1;
+                    db.Entry(interactedpost).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Content("1");
+                }
+                else
+                {
+                    interactedpost.Like = 0;
+                    db.Entry(interactedpost).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Content("-1");
+                }
 
-            return View();
+            }
+            else
+            {
+                db.IneractedPosts.Add(react);
+                db.SaveChanges();
+                return Content("1");
+            }
+
         }
+        public ActionResult disLike(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            var react = new InteractedPosts();
+            react.PostId = post.Id;
+            react.UserId = User.Identity.GetUserId();
+            react.Like = 1;
+            var interactedpost = db.IneractedPosts.FirstOrDefault(x => x.UserId == react.UserId && x.PostId == react.PostId);
+            if (interactedpost != null)
+            {
+                if (interactedpost.Like == 2)
+                {
+                    interactedpost.Like = 0;
+                    db.Entry(interactedpost).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Content("-1");
+                }
+                else if (interactedpost.Like == 0)
+                {
+                    interactedpost.Like = 2;
+                    db.Entry(interactedpost).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Content("1");
+                }
+                else
+                {
+                    interactedpost.Like = 2;
+                    db.Entry(interactedpost).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Content("1");
+                }
+
+            }
+            else
+            {
+                db.IneractedPosts.Add(react);
+                db.SaveChanges();
+                return Content("1");
+            }
+
+        }
+        public ActionResult Save(int id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            var savedPost = new SavedPosts();
+            savedPost.PostId = id;
+            savedPost.UserId = User.Identity.GetUserId();
+            var savedPostInDb = db.SavedPosts.FirstOrDefault(x => x.UserId == savedPost.UserId && x.PostId == savedPost.PostId) as SavedPosts;
+            if (savedPostInDb == null)
+            {
+                db.SavedPosts.Add(savedPost);
+                db.SaveChanges();
+                return Content("Saved");
+            }
+            else
+            {
+                db.SavedPosts.Remove(savedPostInDb);
+                db.SaveChanges();
+                return Content("UnSaved");
+            }
+
+
+        }
+
 
         protected override void Dispose(bool disposing)
         {
---
+++ Press_Agency_System/Models/PostDetailsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Press_Agency_System.Models
+{
+    public class PostDetailsViewModel
+    {
+        public List<InteractedPosts> allInteractions;
+
+        public Post Post { get; set; }
+
+        public byte likedbyuser { get; set; }
+
+        public bool saveedbyuser { get; set; }
+    }
+}
